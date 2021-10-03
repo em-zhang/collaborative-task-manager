356c056f96bc9b3cde739652543f228c535cf575
em-zhang <ezhang0821@gmail.com>
2021-10-02 17:50:03 -0700

    update app.js and css

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
+
+const data = [
+    {
+        id: 512,
+        name: "Neil Rhodes",
+        email: "rhodes@hmc.edu",
+        phone: "(909) 555-1212"
+    },
+    {
+        id: 787,
+        name: "Barack Obama",
+        email: "ex-prez@whitehouse.gov",
+        phone: "(312) 555-1212"
+    }
+];
+
+
+function Person(props) {
+    let className ="person";
+    const [fullOpacity, setFullOpacity] = useState(false);
+    useEffect(() => {
+        if (props.selected) {
+            const timer = setTimeout(() => setFullOpacity(!fullOpacity), 1000);
+            return () => clearTimeout(timer);
+        }
+
+    });
+    return <div id={props.id} className={props.selected ? (fullOpacity? "selectedPerson100" : "selectedPerson50") : className} onClick={props.onPersonClicked}>
+        <div className="name">{props.name}</div>
+        <div className="email">{props.email}</div>
+        <div className="phone">{props.phone}</div>
+    </div>
+}
+
+function People(props) {
+
+    const [selectedID, setSelectedID] = useState(null);
+    const persons = props.peopleList.map(e => <Person onPersonClicked={() => setSelectedID(e.id)}name={e.name} email={e.email}
+                                                      phone={e.phone} id={e.id} selected={selectedID === e.id}/>);
+    return <div>
+        <h1> Task Manager </h1>
+            <h2> Tasks selected {selectedID ? 1 :0}/{data.length} selected</h2>
+        {persons}
+    </div>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <People peopleList={data} />
+        </div>
+    );
 }
 
 export default App;
