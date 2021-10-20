dcf0056127e2fbb94c87c64ba461228fb4ec17aa
Christopher Chung <cchungpom@gmail.com>
2021-10-20 13:38:28 -0700

    Added Firebase config

--- app/src/App.js
+++ app/src/App.js
@@ -4,8 +4,24 @@ import ToDoList from "./components/ToDoList"
 import AddTask from "./components/AddTask"
 import ToolBar from "./components/ToolBar"
 import React, {useState} from "react";
+import firebase from "firebase/compat";
+
+// Config provided from lab docs
+const firebaseConfig = {
+    apiKey: "AIzaSyCd9qqxvMpEKpBzwfWcc2tlRFa6ICaLH_s",
+    authDomain: "hmc-cs124-fa21-labs.firebaseapp.com",
+    projectId: "hmc-cs124-fa21-labs",
+    storageBucket: "hmc-cs124-fa21-labs.appspot.com",
+    messagingSenderId: "949410042946",
+    appId: "1:949410042946:web:0113b139a7e3cd1cc709db"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 function App(props) {
+    // Firebase collection
+    const collectionName = "em-zhang-tasks"
+
     const[showCompleted, setShowCompleted] = useState(true);
     const filteredList = props.taskList.filter(task => showCompleted || !task.isCompleted);
     const numCompleted = props.taskList.filter(task => task.isCompleted).length;
