964fdc315bf3564c6d46df3451639da32b7c2b51
em-zhang <ezhang0821@gmail.com>
2021-12-09 17:47:23 -0800

    stuff

--- app/src/components/SignUp.js
+++ app/src/components/SignUp.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import './SignUp.css'
 
 function SignUp(props) {
     const [email, setEmail] = useState("");
